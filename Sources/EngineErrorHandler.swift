8b0dd995a2cbf6651bbd5e46fe24e22ba842a941
Vadym Markov <markov.vadym@gmail.com>
2016-09-09 14:04:42 +0200

    Add error handler init

--- Sources/EngineErrorHandler.swift
+++ Sources/EngineErrorHandler.swift
@@ -2,6 +2,8 @@ import Aftermath
 
 public struct EngineErrorHandler: ErrorHandler {
 
+  public init() {}
+
   public func handleError(error: ErrorType) {
     if let error = error as? Error {
       log("Engine error: \(error)")
