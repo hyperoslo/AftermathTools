7667353e6a22bf9a3d221878197b1ff7784eb35e
Vadym Markov <markov.vadym@gmail.com>
2016-09-10 22:02:36 +0200

    Add error type

--- Sources/Helpers.swift
+++ Sources/Helpers.swift
@@ -1,3 +1,20 @@
+// MARK: - Errors
+
+public struct Error: ErrorType, CustomStringConvertible, CustomDebugStringConvertible {
+
+  public let description: String
+
+  public init(description: String) {
+    self.description = description
+  }
+
+  public var debugDescription: String {
+    return description
+  }
+}
+
+// MARK: - Functions
+
 func log(text: String) {
   print("🔮 AFTERMATH: \(text)")
 }
