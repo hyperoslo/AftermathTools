bf8e872fd677484b7772d068b466439014d7dc3a
Vadym Markov <markov.vadym@gmail.com>
2016-09-10 22:14:37 +0200

    Silence command middleware

--- Sources/CommandMiddleware.swift
+++ Sources/CommandMiddleware.swift
@@ -67,3 +67,22 @@ public struct AbortCommandMiddleware: CommandMiddleware {
     try next(command)
   }
 }
+
+// MARK: - Abort Event Middleware
+
+public struct SilenceCommandMiddleware: CommandMiddleware {
+
+  var error: ErrorType?
+
+  public init(error: ErrorType? = nil) {
+    self.error = error
+  }
+
+  public func intercept(command: AnyCommand, execute: Execute, next: Execute) throws {
+    log("Silence mode: event has been blocked -> \(command)")
+
+    if let error = error {
+      throw error
+    }
+  }
+}
