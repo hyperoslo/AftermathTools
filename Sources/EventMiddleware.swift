6c777d530020494cc7d888efa3994f670ea1ce84
Vadym Markov <markov.vadym@gmail.com>
2016-09-10 22:14:47 +0200

    Silence event middleware

--- Sources/EventMiddleware.swift
+++ Sources/EventMiddleware.swift
@@ -67,3 +67,22 @@ public struct AbortEventMiddleware: EventMiddleware {
     try next(event)
   }
 }
+
+// MARK: - Abort Event Middleware
+
+public struct SilenceEventMiddleware: EventMiddleware {
+
+  var error: ErrorType?
+
+  public init(error: ErrorType? = nil) {
+    self.error = error
+  }
+
+  public func intercept(event: AnyEvent, publish: Publish, next: Publish) throws {
+    log("Silence mode: event has been blocked -> \(event)")
+
+    if let error = error {
+      throw error
+    }
+  }
+}
