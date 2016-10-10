498ee050864b507b0a58003045bb08a0631329af
Christoffer Winterkvist <christoffer@hyper.no>
2016-10-10 11:21:33 +0200

    Remove Failure struct

--- Sources/Helpers.swift
+++ Sources/Helpers.swift
@@ -1,18 +1,3 @@
-// MARK: - Errors
-
-public struct Failure: Error, CustomStringConvertible, CustomDebugStringConvertible {
-
-  public let description: String
-
-  public init(description: String) {
-    self.description = description
-  }
-
-  public var debugDescription: String {
-    return description
-  }
-}
-
 // MARK: - Functions
 
 func log(_ text: String) {
